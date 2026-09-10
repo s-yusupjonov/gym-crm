ecb78b877610a2afa627855fa4f7a3cd6c7e096d
Sanjar Yusupjonov <s.yusupjponov@newuu.uz>
2026-09-10 13:35:15 +0500

    fix required Http method restrict and add Component to TransactionIdFilter class

--- src/main/java/com/gym/crm/config/SecurityConfig.java
+++ src/main/java/com/gym/crm/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/trainees", "/api/trainers").permitAll()
-                        .requestMatchers("/api/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
